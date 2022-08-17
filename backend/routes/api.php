a4d5ec20f9b7920f05dcc32b13155b9831b87ab5
LeiderGalindo29 <104436225+LeiderGalindo29@users.noreply.github.com>
2022-08-17 11:52:35 -0500

    Rutas Postman

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BootcampController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
+
+//primera ruta
+Route::get('prueba', function(){echo "hola";});
+
+//vincular controlador bootcamp a sus respectivas rutas
+
+Route::apiResource('bootcamps',BootcampController::class);
